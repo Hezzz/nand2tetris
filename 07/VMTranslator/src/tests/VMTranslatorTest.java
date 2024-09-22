b189875707f00fe8f71299556b68b2c7de0f3668
Hezzz <49769113+Hezzz@users.noreply.github.com>
2024-09-22 16:07:08 +0800

    Added all tests

--- 07/VMTranslator/src/tests/VMTranslatorTest.java
+++ 07/VMTranslator/src/tests/VMTranslatorTest.java
@@ -21,6 +21,50 @@ public class VMTranslatorTest {
         Assert.assertEquals(tBasicTestAsm, basicTestAsm);
     }
 
+    @Test
+    public void pointerTest(){
+        // Arrange
+        String tPointerTestAsm = readFile("../../07/PointerTest/PointerTest.test.asm");
+        // Act
+        new VMTranslator("../../07/PointerTest/PointerTest.vm");
+        String pointerTestAsm = readFile("../../07/PointerTest/PointerTest.asm");
+        // Assert
+        Assert.assertEquals(tPointerTestAsm, pointerTestAsm);
+    }
+
+    @Test
+    public void simpleAdd(){
+        // Arrange
+        String tSimpleAddAsm = readFile("../../07/SimpleAdd/SimpleAdd.test.asm");
+        // Act
+        new VMTranslator("../../07/SimpleAdd/SimpleAdd.vm");
+        String simpleAddAsm = readFile("../../07/SimpleAdd/SimpleAdd.asm");
+        // Assert
+        Assert.assertEquals(tSimpleAddAsm, simpleAddAsm);
+    }
+
+    @Test
+    public void stackTest(){
+        // Arrange
+        String tStackTestAsm = readFile("../../07/StackTest/StackTest.test.asm");
+        // Act
+        new VMTranslator("../../07/StackTest/StackTest.vm");
+        String stackTestAsm = readFile("../../07/StackTest/StackTest.asm");
+        // Assert
+        Assert.assertEquals(tStackTestAsm, stackTestAsm);
+    }
+
+    @Test
+    public void staticTest(){
+        // Arrange
+        String tStaticTestAsm = readFile("../../07/StaticTest/StaticTest.test.asm");
+        // Act
+        new VMTranslator("../../07/StaticTest/StaticTest.vm");
+        String staticTestAsm = readFile("../../07/StaticTest/StaticTest.asm");
+        // Assert
+        Assert.assertEquals(tStaticTestAsm, staticTestAsm);
+    }
+
     private String readFile(String filePath) {
         String content = "";
         try {
