cae190ed1dca4e978420bbac8111ecef2b20ae89
Hezzz <49769113+Hezzz@users.noreply.github.com>
2025-04-19 19:52:39 +0800

    Refactored for readability

--- 07/VMTranslator/src/vmtranslator/CodeWriter.java
+++ 07/VMTranslator/src/vmtranslator/CodeWriter.java
@@ -7,13 +7,13 @@ import java.io.IOException;
 public class CodeWriter {
 
     private FileWriter writer;
-    private final String outputFileName;       
+    private final String outputFileName;
     // for comparison label
     private int eq_count = 0;
     private int gt_count = 0;
     private int lt_count = 0;
 
-    public CodeWriter(File outputFile){
+    public CodeWriter(File outputFile) {
         this.outputFileName = outputFile.getName().substring(0, outputFile.getName().lastIndexOf("."));
         try {
             this.writer = new FileWriter(outputFile);
@@ -24,193 +24,238 @@ public class CodeWriter {
         }
     }
 
-    public void writeArithmetic(String command){
+    public void writeArithmetic(String command) {
         try {
             writer.append("// " + command).append(System.lineSeparator());
-            String commands = performArithmetic(command);
-            writer.append(commands);
+            String arithmeticCommands = generateArithmeticCommands(command);
+            writer.append(arithmeticCommands);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private String performArithmetic(String command){
+    public void writePushPop(CommandType commandType, String segment, int index) {
+        String command = commandType == CommandType.C_POP ? "pop" : "push";
+        try {
+            // add comment
+            writer.append("// " + command + " " + segment + " " + index).append(System.lineSeparator());
+            String pushPopCommands = commandType == CommandType.C_POP ? pop(segment, index) : push(segment, index);
+            writer.append(pushPopCommands);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String generateArithmeticCommands(String command) {
         StringBuilder arithmeticCommand = new StringBuilder();
         // pop top of the stack
-        arithmeticCommand.append(popItem());
-        // pop 2nd item
+        arithmeticCommand.append(popStack());
+        // pop next item from stack
         if (!command.equals("not") && !command.equals("neg")) {
-            // assign item to D
-            arithmeticCommand.append("D=M").append(System.lineSeparator());
-            arithmeticCommand.append(popItem());
+            // assign item to D register
+            appendLine(arithmeticCommand, "D=M");
+            arithmeticCommand.append(popStack());
         }
-        // if not comparison operator, compute
-        if (!command.equals("eq") && !command.equals("lt") && !command.equals("gt")){
-            arithmeticCommand.append(compute(command)).append(System.lineSeparator());
+        // if not comparison command, perform binary operations
+        if (!command.equals("eq") && !command.equals("lt") && !command.equals("gt")) {
+            String binaryOperationCommands = getBinaryOperationCommands(command);
+            appendLine(arithmeticCommand, binaryOperationCommands);
             arithmeticCommand.append(pushStackPointer());
         } else {
-            arithmeticCommand.append(compare(command)).append(System.lineSeparator());
+            String comparisonCommands = getComparisonCommands(command);
+            appendLine(arithmeticCommand, comparisonCommands);
         }
         return arithmeticCommand.toString();
     }
 
-    private String compare(String command) {
-        StringBuilder compareCommand = new StringBuilder();
-        // check equality to 0
-        compareCommand.append("D=M-D").append(System.lineSeparator());
-        // set value of top of stack to false
-        compareCommand.append("M=0").append(System.lineSeparator());
-        // do compare
-        String label = getLabel(command);
-        compareCommand.append("@" + label).append(System.lineSeparator());
-        compareCommand.append(getCompare(command)).append(System.lineSeparator());
-        // set value of top of stack to true if compare is true 
-        compareCommand.append("@SP").append(System.lineSeparator());
-        compareCommand.append("A=M").append(System.lineSeparator());
-        compareCommand.append("M=-1").append(System.lineSeparator());
+    private String getComparisonCommands(String command) {
+        StringBuilder tempBuilder = new StringBuilder();
+        // check equality of M and D registers to 0; result is stored in D register
+        appendLine(tempBuilder, "D=M-D");
+        // set value of top of stack to false; default value
+        appendLine(tempBuilder, "M=0");
+        // generate comparison sequence
+        String label = getComparisonLabel(command);
+        appendLine(tempBuilder, "@" + label);
+        String comparisonOperationCommands = getComparisonOperationCommands(command);
+        appendLine(tempBuilder, comparisonOperationCommands);
+        // set value of top of stack to true if compare is true
+        appendLine(tempBuilder, "@SP");
+        appendLine(tempBuilder, "A=M");
+        appendLine(tempBuilder, "M=-1");
         // add label here if compare is false; jump here
-        compareCommand.append("(" + label + ")").append(System.lineSeparator());
-        compareCommand.append("@SP").append(System.lineSeparator());
-        compareCommand.append("M=M+1");
-        return compareCommand.toString();
-    }
-
-    private String getLabel(String command) {
-        if (command.equals("eq")) return "EQ_" + eq_count++;
-        if (command.equals("gt")) return "GT_" + gt_count++;
-        if (command.equals("lt")) return "LT_" + lt_count++;
-        else return "";
-    }
-
-    private String getCompare(String command){
-        if (command.equals("eq")) return "D;JNE";
-        if (command.equals("gt")) return "D;JLE";
-        if (command.equals("lt")) return "D;JGE";
-        else return "";
-    }
-
-    private String compute(String command){
-        if (command.equals("neg")) return "D=-M";
-        if (command.equals("not")) return "D=!M";
-        if (command.equals("and")) return "D=D&M";
-        if (command.equals("or")) return "D=D|M";
-        if (command.equals("add")) return "D=D+M";
-        if (command.equals("sub")) return "D=M-D";
-        else return "";
-    }
-
-    private String popItem(){
-        return new StringBuilder()
-                    .append("@SP").append(System.lineSeparator())
-                    .append("AM=M-1").append(System.lineSeparator())
-                    .toString();
-    }
-
-    public void writePushPop(CommandType commandType, String segment, int index){
-        String command = commandType == CommandType.C_POP ? "pop" : "push";
-        try {
-            // add comment 
-            writer.append("// " + command + " " + segment + " " + index).append(System.lineSeparator());
-            String commands = commandType == CommandType.C_POP ? pop(segment, index) : push(segment, index);
-            writer.append(commands);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        appendLine(tempBuilder, "(" + label + ")");
+        appendLine(tempBuilder, "@SP");
+        tempBuilder.append("M=M+1");
+        return tempBuilder.toString();
     }
 
     private String push(String segment, int index) {
-        StringBuilder pushCommand = new StringBuilder();
-        // initValueialize the address value
-        pushCommand.append(initValue(segment, index));
-        // get address
-        if (!segment.equals("constant")) 
-            pushCommand.append(getAddress(segment, index)).append(System.lineSeparator());
-        if (segment.equals("temp")) 
-            pushCommand.append("A=D+A").append(System.lineSeparator());
+        StringBuilder tempBuilder = new StringBuilder();
+        // initialize the address value
+        tempBuilder.append(initializeValue(segment, index));
+        // get current address of segments except `constant`
+        if (!segment.equals("constant")) {
+            String address = getAddress(segment, index);
+            appendLine(tempBuilder, address);
+        }
+        // temp segment
+        if (segment.equals("temp"))
+            appendLine(tempBuilder, "A=D+A");
+        // local, argument, this, that segments
         if (segment.equals("local") || segment.equals("argument") || segment.equals("this") || segment.equals("that"))
-            pushCommand.append("A=D+M").append(System.lineSeparator());
-        // assign address to D reg
-        if (!segment.equals("constant")) 
-            pushCommand.append("D=M").append(System.lineSeparator());
+            appendLine(tempBuilder, "A=D+M");
+        // assign segment address to D register except `constant`
+        if (!segment.equals("constant"))
+            appendLine(tempBuilder, "D=M");
         // increment stack pointer
-        pushCommand.append(pushStackPointer());
-        return pushCommand.toString();
+        tempBuilder.append(pushStackPointer());
+        return tempBuilder.toString();
     }
 
     private String pop(String segment, int index) {
-        StringBuilder popCommand = new StringBuilder();
-        // initValueialize the address value
-        popCommand.append(initValue(segment, index));
-        // get address
+        StringBuilder tempBuilder = new StringBuilder();
+        // initialize the address value
+        tempBuilder.append(initializeValue(segment, index));
+        // get current address of segments
         String address = getAddress(segment, index);
-        if (!segment.equals("static") && !segment.equals("pointer")) 
-            popCommand.append(address).append(System.lineSeparator());
-        if (segment.equals("temp")) 
-            popCommand.append("D=D+A").append(System.lineSeparator());
+        // if segment is not static or pointer, load address to memory
+        if (!segment.equals("static") && !segment.equals("pointer"))
+            appendLine(tempBuilder, address);
+        // if temp segment, add 5 to address
+        if (segment.equals("temp"))
+            appendLine(tempBuilder, "D=D+A");
+        // if local, argument, this, that segments, add M to address
         if (segment.equals("local") || segment.equals("argument") || segment.equals("this") || segment.equals("that"))
-            popCommand.append("D=D+M").append(System.lineSeparator());
-        // assign address to @R15 if not static and pointer
+            appendLine(tempBuilder, "D=D+M");
+        // assign address to @R15 if segment is not static and pointer
         if (!segment.equals("static") && !segment.equals("pointer")) {
-            popCommand.append("@R15").append(System.lineSeparator());
-            popCommand.append("M=D").append(System.lineSeparator());
+            appendLine(tempBuilder, "@R15");
+            appendLine(tempBuilder, "M=D");
         }
         // decrement stack pointer
-        popCommand.append(popStackPointer());
+        tempBuilder.append(popStackPointer());
         if (!segment.equals("static") && !segment.equals("pointer")) {
-            popCommand.append("@R15").append(System.lineSeparator());
-            popCommand.append("A=M").append(System.lineSeparator());
+            appendLine(tempBuilder, "@R15");
+            appendLine(tempBuilder, "A=M");
         } else {
-            popCommand.append(address).append(System.lineSeparator());
+            appendLine(tempBuilder, address);
         }
-        popCommand.append("M=D").append(System.lineSeparator());
-        return popCommand.toString();
+        appendLine(tempBuilder, "M=D");
+        return tempBuilder.toString();
     }
 
-    private String initValue(String segment, int index){
+    private String initializeValue(String segment, int index) {
         if (!segment.equals("static") && !segment.equals("pointer")) {
-            return new StringBuilder()
-                        .append("@" + index).append(System.lineSeparator())
-                        .append("D=A").append(System.lineSeparator())
-                        .toString();
+            StringBuilder tempBuilder = new StringBuilder();
+            appendLine(tempBuilder, "@" + index);
+            appendLine(tempBuilder, "D=A");
+            return tempBuilder.toString();
         }
         return "";
     }
 
-    private String getAddress(String segment, int index){
-        if (segment.equals("temp")) return "@5";
-        if (segment.equals("local")) return "@LCL";
-        if (segment.equals("argument")) return "@ARG";
-        if (segment.equals("this")) return "@THIS";
-        if (segment.equals("that")) return "@THAT";
-        if (segment.equals("static")) return "@" + outputFileName + "." + index;
-        if (segment.equals("pointer")) return index == 0 ? "@THIS" : "@THAT";
-        return "";
+    private String getAddress(String segment, int index) {
+        switch (segment) {
+            case "temp":
+                return "@5";
+            case "local":
+                return "@LCL";
+            case "argument":
+                return "@ARG";
+            case "this":
+                return "@THIS";
+            case "that":
+                return "@THAT";
+            case "static":
+                return "@" + outputFileName + "." + index;
+            case "pointer":
+                return index == 0 ? "@THIS" : "@THAT";
+            default:
+                return "";
+        }
     }
 
-    private String pushStackPointer(){
-        return new StringBuilder()
-                    .append("@SP").append(System.lineSeparator())
-                    .append("A=M").append(System.lineSeparator())
-                    .append("M=D").append(System.lineSeparator())
-                    .append("@SP").append(System.lineSeparator())
-                    .append("M=M+1").append(System.lineSeparator())
-                    .toString();
+    private String getComparisonLabel(String command) {
+        switch (command) {
+            case "eq":
+                return "EQ_" + eq_count++;
+            case "gt":
+                return "GT_" + gt_count++;
+            case "lt":
+                return "LT_" + lt_count++;
+            default:
+                return "";
+        }
     }
 
-    private String popStackPointer(){
-        return new StringBuilder()
-                    .append("@SP").append(System.lineSeparator())
-                    .append("M=M-1").append(System.lineSeparator())
-                    .append("A=M").append(System.lineSeparator())
-                    .append("D=M").append(System.lineSeparator())
-                    .toString();
+    private static String getComparisonOperationCommands(String command) {
+        switch (command) {
+            case "eq":
+                return "D;JNE";
+            case "gt":
+                return "D;JLE";
+            case "lt":
+                return "D;JGE";
+            default:
+                return "";
+        }
     }
 
-    public void close(){
+    private static String getBinaryOperationCommands(String command) {
+        switch (command) {
+            case "neg":
+                return "D=-M";
+            case "not":
+                return "D=!M";
+            case "and":
+                return "D=D&M";
+            case "or":
+                return "D=D|M";
+            case "add":
+                return "D=D+M";
+            case "sub":
+                return "D=M-D";
+            default:
+                return "";
+        }
+    }
+
+    private static String pushStackPointer() {
+        StringBuilder tempBuilder = new StringBuilder();
+        appendLine(tempBuilder, "@SP");
+        appendLine(tempBuilder, "A=M");
+        appendLine(tempBuilder, "M=D");
+        appendLine(tempBuilder, "@SP");
+        appendLine(tempBuilder, "M=M+1");
+        return tempBuilder.toString();
+    }
+
+    private static String popStackPointer() {
+        StringBuilder tempBuilder = new StringBuilder();
+        appendLine(tempBuilder, "@SP");
+        appendLine(tempBuilder, "M=M-1");
+        appendLine(tempBuilder, "A=M");
+        appendLine(tempBuilder, "D=M");
+        return tempBuilder.toString();
+    }
+
+    private static String popStack() {
+        StringBuilder tempBuilder = new StringBuilder();
+        appendLine(tempBuilder, "@SP");
+        appendLine(tempBuilder, "AM=M-1");
+        return tempBuilder.toString();
+    }
+
+    public void close() {
         try {
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void appendLine(StringBuilder sb, String line) {
+        sb.append(line).append(System.lineSeparator());
     }
 }
