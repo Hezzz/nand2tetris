705278d63d46f0a5d0be70f08dd82dd0492b0764
Hezzz <49769113+Hezzz@users.noreply.github.com>
2025-06-23 22:59:25 +0800

    Added project 8 tests for VM Translator

--- 07/VMTranslator/src/tests/VMTranslatorTest.java
+++ 07/VMTranslator/src/tests/VMTranslatorTest.java
@@ -11,7 +11,7 @@ import vmtranslator.VMTranslator;
 public class VMTranslatorTest {
 
     @Test
-    public void basicTest(){
+    public void basicTest() {
         // Arrange
         String tBasicTestAsm = readFile("../../07/BasicTest/BasicTest.test.asm");
         // Act
@@ -22,7 +22,7 @@ public class VMTranslatorTest {
     }
 
     @Test
-    public void pointerTest(){
+    public void pointerTest() {
         // Arrange
         String tPointerTestAsm = readFile("../../07/PointerTest/PointerTest.test.asm");
         // Act
@@ -33,7 +33,7 @@ public class VMTranslatorTest {
     }
 
     @Test
-    public void simpleAdd(){
+    public void simpleAdd() {
         // Arrange
         String tSimpleAddAsm = readFile("../../07/SimpleAdd/SimpleAdd.test.asm");
         // Act
@@ -44,7 +44,7 @@ public class VMTranslatorTest {
     }
 
     @Test
-    public void stackTest(){
+    public void stackTest() {
         // Arrange
         String tStackTestAsm = readFile("../../07/StackTest/StackTest.test.asm");
         // Act
@@ -55,7 +55,7 @@ public class VMTranslatorTest {
     }
 
     @Test
-    public void staticTest(){
+    public void staticTest() {
         // Arrange
         String tStaticTestAsm = readFile("../../07/StaticTest/StaticTest.test.asm");
         // Act
@@ -63,6 +63,72 @@ public class VMTranslatorTest {
         String staticTestAsm = readFile("../../07/StaticTest/StaticTest.asm");
         // Assert
         Assert.assertEquals(tStaticTestAsm, staticTestAsm);
+    }
+
+    @Test
+    public void basicLoop() {
+        // Arrange
+        String tBasicLoopAsm = readFile("../../07/BasicLoop/BasicLoop.test.asm");
+        // Act
+        new VMTranslator("../../07/BasicLoop/BasicLoop.vm");
+        String basicLoopAsm = readFile("../../07/BasicLoop/BasicLoop.asm");
+        // Assert
+        Assert.assertEquals(tBasicLoopAsm, basicLoopAsm);
+    }
+
+    @Test
+    public void fibonacciElement() {
+        // Arrange
+        String tFibonacciElementAsm = readFile("../../07/FibonacciElement/FibonacciElement.test.asm");
+        // Act
+        new VMTranslator("../../07/FibonacciElement");
+        String fibonacciElementAsm = readFile("../../07/FibonacciElement/FibonacciElement.asm");
+        // Assert
+        Assert.assertEquals(tFibonacciElementAsm, fibonacciElementAsm);
+    }
+
+    @Test
+    public void fibonacciSeries() {
+        // Arrange
+        String tFibonacciSeriesAsm = readFile("../../07/FibonacciSeries/FibonacciSeries.test.asm");
+        // Act
+        new VMTranslator("../../07/FibonacciSeries/FibonacciSeries.vm");
+        String fibonacciSeriesAsm = readFile("../../07/FibonacciSeries/FibonacciSeries.asm");
+        // Assert
+        Assert.assertEquals(tFibonacciSeriesAsm, fibonacciSeriesAsm);
+    }
+
+    @Test
+    public void NestedCall() {
+        // Arrange
+        String tNestedCallAsm = readFile("../../07/NestedCall/NestedCall.test.asm");
+        // Act
+        new VMTranslator("../../07/NestedCall");
+        String nestedCallAsm = readFile("../../07/NestedCall/NestedCall.asm");
+        // Assert
+        Assert.assertEquals(tNestedCallAsm, nestedCallAsm);
+    }
+
+    @Test
+    public void SimpleFunction() {
+        // Arrange
+        String tSimpleFunctionAsm = readFile("../../07/SimpleFunction/SimpleFunction.test.asm");
+        // Act
+        new VMTranslator("../../07/SimpleFunction/SimpleFunction.vm");
+        String simpleFunctionAsm = readFile("../../07/SimpleFunction/SimpleFunction.asm");
+        // Assert
+        Assert.assertEquals(tSimpleFunctionAsm, simpleFunctionAsm);
+    }
+
+    @Test
+    public void StaticsTest() {
+        // Arrange
+        String tStaticsTestAsm = readFile("../../07/StaticsTest/StaticsTest.test.asm");
+        // Act
+        new VMTranslator("../../07/StaticsTest");
+        String staticsTestAsm = readFile("../../07/StaticsTest/StaticsTest.asm");
+        // Assert
+        Assert.assertEquals(tStaticsTestAsm, staticsTestAsm);
     }
 
     private String readFile(String filePath) {
